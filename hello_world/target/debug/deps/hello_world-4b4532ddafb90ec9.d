29462ab201d27d37d7b8d06df0d5edff68e66be2
BlueFlame_Fox <joarmstrong2014@gmail.com>
2018-10-07 14:03:03 +0100

    Hello_World
    my first pice of rust code

---
+++ hello_world/target/debug/deps/hello_world-4b4532ddafb90ec9.d
@@ -0,0 +1,5 @@
+C:\Users\Joarm\Documents\Rust\hello_world\target\debug\deps\hello_world-4b4532ddafb90ec9.exe: src\main.rs
+
+C:\Users\Joarm\Documents\Rust\hello_world\target\debug\deps\hello_world-4b4532ddafb90ec9.d: src\main.rs
+
+src\main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+C:\Users\Joarm\Documents\Rust\hello_world\target\debug\hello_world.pdb: C:\Users\Joarm\Documents\Rust\hello_world\src\main.rs
