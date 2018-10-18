f9b368d18511e48aa01d12515ff33ba04f09a14d
BlueFlame_Fox <joarmstrong2014@gmail.com>
2018-10-18 20:37:11 +0100

    arma 3 sim
    update 1

--- Arma_3_simulator/src/main.rs
+++ Arma_3_simulator/src/main.rs
@@ -4,19 +4,25 @@ fn main() {
     println!("Hello to the game of games, the simulators of simulators ");
     println!("Welcome to The Arma 3 simulator");
     println!("Are you Ready?");
-    let input = input();
-    println!("{}", input);
-    match input {};
+    println!("1) Yes");
+    println!("2) No");
+    let choice1 = input_n();
+    match choice1 {
+        1 => println!("Ok well lets go!"),
+        2 => println!("Well tuff you have to"),
+        _ => panic!("You enterd a unknown character"),
+    };
+    println!("You are in command of a squad and you have to make sure they live");
 }
 
-fn input() -> i32 {
-    let mut input = String::new();
+fn input_n() -> i32 {
+    let mut input_n = String::new();
     io::stdin()
-        .read_line(&mut input)
+        .read_line(&mut input_n)
         .expect("You failed at life :(");
-    let input: i32 = match input.trim().parse() {
+    let input_n: i32 = match input_n.trim().parse() {
         Ok(num) => num,
         Err(_) => panic!("IM PANICING!"),
     };
-    input
+    input_n
 }
