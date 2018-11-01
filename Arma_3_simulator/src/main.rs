25ec98ea42640fd3cbedf59ce01444c4789e84aa
BlueFlame-Fox <joarmstrong2014@gmail.com>
2018-11-01 11:26:56 -0700

    text based game
    part 3

--- Arma_3_simulator/src/main.rs
+++ Arma_3_simulator/src/main.rs
@@ -1,28 +1,50 @@
 use std::io;
-
+extern crate read_input;
+use read_input::*;
 fn main() {
+    let mut money = 0;
     println!("Hello to the game of games, the simulators of simulators ");
     println!("Welcome to The Arma 3 simulator");
     println!("Are you Ready?");
     println!("1) Yes");
     println!("2) No");
-    let choice1 = input_n();
-    match choice1 {
-        1 => println!("Ok well lets go!"),
-        2 => println!("Well tuff you have to"),
-        _ => panic!("You enterd a unknown character"),
-    };
-    println!("You are in command of a squad and you have to make sure they live");
-}
 
-fn input_n() -> i32 {
-    let mut input_n = String::new();
-    io::stdin()
-        .read_line(&mut input_n)
-        .expect("You failed at life :(");
-    let input_n: i32 = match input_n.trim().parse() {
-        Ok(num) => num,
-        Err(_) => panic!("IM PANICING!"),
+    match valid_input(&|x| *x < 3 && *x > 0) {
+        1 => println!("Ok well lets go!"),
+        2 => println!("Well you chose to play this game so lets go!"),
+        _ => panic!("You entered a unknown character"),
     };
-    input_n
+    println!("You are in command of a squad of 5 and you have to make sure they live.");
+    println!("What is your surname");
+    let name: String = simple_input();
+
+    println!("Hello commander {}", name);
+    println!("Your Squad mates are called: Mark, Jose, arthur, Dutch and John");
+    println!("crossroads: We are going to send your team on a recon mission.");
+    println!("Crossroads: You will be undercover as citizens.");
+    println!("Crossroads: I will let you make the choices lets go.");
+    println!("Crossroads: How do you want to get there?");
+
+    match simple_input() {
+        1 => {
+            println!("Crossroad: Copy that we will send the Tank to you now. ");
+            println!("Crossroad: Drive as near the enemy base without being detectied");
+            println!("You go near the enemy base but the enemy hear the tank");
+            println!("They get out the Anti-Tank launcher and you are about to load the tank but your too slow and the tank is destroyed");
+            println!("Mission Failed");
+            println!("We'll get them next time.");
+            println!("Do you want to Play again");
+            match valid_input(&|x| *x < 3 && *x > 0) {
+                1 => main(),
+                _ => (),
+            }
+        }
+        2 => {
+            println!("Crossroads: Copy that we wil send the truck to you now.");
+        }
+        3 => {
+            println!("Crossroads: Copy that we will send the Jeep to you now");
+        }
+        _ => panic!("You enterd a unknown character"),
+    }
 }
